5d6caf784ebe81027ae36384f45d17337f754e10
aga-ga-ga <sasha_asp@list.ru>
2014-05-26 19:44:54 +0300

    add Caclulate function

--- calc.c
+++ calc.c
@@ -16,7 +16,6 @@ static char arg1_input[WRITE_SIZE];
 static char arg2_input[WRITE_SIZE];
 static char operation_input[WRITE_SIZE];
 
-long calculate(void) ;
 
 
 
@@ -25,6 +24,32 @@ struct proc_dir_entry *arg1;
 struct proc_dir_entry *arg2;
 struct proc_dir_entry *operation;
 struct proc_dir_entry *result;
+
+
+long calculate(void) {
+	long a1 = 0;
+	long a2 = 0;
+	long res = 0;
+
+	if (arg1_input[strlen(arg1_input) - 2] == '\n') {
+		arg1_input[strlen(arg1_input) - 2] = (char)0;
+	}
+	//string -> long
+	kstrtol(arg1_input, 10, &a1);
+	kstrtol(arg2_input, 10, &a2);
+
+	if (operation_input[0] == '+') {
+		res = a1 + a2;
+	} else if (operation_input[0] == '-') {
+		res = a1 - a2;
+	} else if (operation_input[0] == '.') {
+		res = a1 * a2;
+	} else if (operation_input[0] == '/') {
+		res = a1 / a2;
+	}
+	return res;
+}
+
 
 
 /*
